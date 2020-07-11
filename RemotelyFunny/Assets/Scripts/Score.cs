146eb5a5066003c2ce6137bcffc969cd3cb21649
edwin-silerio <edwin_silerio@utexas.edu>
2020-07-11 00:21:48 -0500

    Added score functionality

---
+++ RemotelyFunny/Assets/Scripts/Score.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+/// <summary>
+/// Changes the player's score
+/// </summary>
+public class Score : MonoBehaviour
+{
+    [SerializeField] private int score = 0;
+    
+    private TextMeshProUGUI scoreDisplay = default;
+    private KeyCode incScore = KeyCode.Period;
+    private KeyCode decScore = KeyCode.Comma;
+
+    /*
+     * Grab the score text
+     */
+    private void Start()
+    {
+        scoreDisplay = GetComponent<TextMeshProUGUI>();
+    }
+
+    /*
+     * Used for debugging. Press the . to increase the score. Press , to dec the score
+     */
+    private void Update()
+    {
+        if (Input.GetKeyDown(incScore))
+        {
+            ChangeScore(10);
+        }
+        else if (Input.GetKeyDown(decScore))
+        {
+            ChangeScore(-10);
+        }
+    }
+
+    /// <summary>
+    /// Adds the int that's passed to the player's score and changes the display
+    /// </summary>
+    /// <param name="value"></param>
+    public void ChangeScore(int value)
+    {
+        score = int.Parse(scoreDisplay.text) + value;
+        scoreDisplay.text = score.ToString();
+    }
+}
