a069b9e6eaae3d1487295c9a9162ff15326bb19c
edwin-silerio <edwin_silerio@utexas.edu>
2020-07-10 23:12:15 -0500

    Created the menu

---
+++ RemotelyFunny/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Handles the menu buttons
+/// </summary>
+public class MainMenu: MonoBehaviour
+{
+    [SerializeField] private float quitDelay = 1f;
+    [SerializeField] private CanvasGroup highscores = default;
+    [SerializeField] private CanvasGroup menu = default;
+
+    /// <summary>
+    /// Goes to the game scene
+    /// </summary>
+    public void GoToGame()
+    {
+        Debug.Log("Playing game");
+        SceneManager.LoadScene("Game");
+    }
+
+    /// <summary>
+    /// Displays the highscores
+    /// </summary>
+    public void ShowScores()
+    {
+        Debug.Log("Showing scores");
+        menu.alpha = 0;
+        menu.blocksRaycasts = false;
+        menu.interactable = false;
+        highscores.alpha = 1;
+        highscores.blocksRaycasts = true;
+        highscores.interactable = true;
+    }
+
+    /// <summary>
+    /// Hides the scores
+    /// </summary>
+    public void HideScores()
+    {
+        Debug.Log("Showing menu");
+        highscores.alpha = 0;
+        highscores.blocksRaycasts = false;
+        highscores.interactable = false;
+        menu.alpha = 1;
+        menu.blocksRaycasts = true;
+        menu.interactable = true;
+    }
+
+    /// <summary>
+    /// Quits the game
+    /// </summary>
+    public void QuitGame()
+    {
+        Debug.Log("Quit the game");
+        StartCoroutine(QuitAfterDelay());
+    }
+
+    private IEnumerator QuitAfterDelay()
+    {
+        yield return new WaitForSeconds(quitDelay);
+        Application.Quit();
+    }
+
+}
---
+++ RemotelyFunny/Assets/Scripts/Utilities/Utilities.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+#if UNITY_EDITOR
+using UnityEditor.SceneManagement;
+#endif
+using UnityEngine.UI;
+
+/// <summary>
+/// Class that has misc functions that Unity doesn't provide
+/// </summary>
+public static class Utilities
+{
+    /*
+     * Add any new scenes here for ease of access in the editor. Follow previous entries' format
+     */
+#if UNITY_EDITOR
+
+    // Main menu
+    [MenuItem("Scenes/Main Menu")] public static void MainMenuScene() => 
+        OpenScene("Assets/Scenes/MainMenu.unity");
+
+    // Game
+    [MenuItem("Scenes/Game")] public static void ScenarioOne() => 
+        OpenScene("Assets/Scenes/Game.unity");
+    
+    [MenuItem("Scenes/Add or Edit Scene Menu Options")] public static void EditMenu() =>
+        AssetDatabase.OpenAsset(AssetDatabase.LoadAssetAtPath("Assets/Scripts/Utility/Utilities.cs", typeof(Object)), 56);
+    private static void OpenScene(string scenePath)
+    {
+        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        EditorSceneManager.OpenScene(scenePath);
+    }
+#endif
+}
